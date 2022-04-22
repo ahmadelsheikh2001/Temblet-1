ddbc42c5853771c8578f5d92d5567e237a64eee7
ahmadelsheikh2001 <ahmadelsheikhali@gmil.com>
2022-04-22 14:33:52 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,166 @@
+// var  data = [
+//     {
+//         username: "ahmed",
+//         skills: [ "htlm" , "css"]
+//     },
+//     {
+//         username: "ali",
+//         skills: [ "j.s" , "react"]
+//     },
+//     {
+//         username: "mohamed",
+//         skills: [ "node" , "Angular"]
+//     }
+// ]
+
+// console.log(data[2].skills[1]);
+
+// var x = 9;
+// x++;    // post increment
+// ++x;    // pre increment
+
+// console.log(--x) ;   // 8
+// console.log(x) ;
+
+// ++ increment
+// -- decrement
+// ///////////////////////////////////////////////////////
+//  +  -  *  /   %  ++  --  **
+// var x = 98;
+// // console.log(x**3)
+
+// console.log(x % 2); //
+// var x = 6 ;
+// console.log ( typeof -false)
+// //////////////////////////////////////////////////////
+// var x = 47;
+
+// if (x >= 85 ) {
+//     console.log("excellent")
+// }
+// else if (x >= 75) {
+//     console.log("Very performance")
+// }
+// else if ( x >= 65 ) {
+//     console.log("performance")
+// }
+// else if ( x >= 50 ) {
+//     console.log("Accepted")
+// }
+// else {
+//     console.log("fail")
+// }
+//////////////////////////////////////////////////////////
+
+// var x = 8 ;
+
+// switch (x) {
+//     case 1 :
+//     console.log("car")
+//     break
+
+//     case 2 :
+//     console.log("LapTop")
+//     break
+
+//     case 3 :
+//     console.log("Mobile")
+//     break
+
+//     default:
+//     console.log (" هزعلك ")
+
+// }
+//////////////////////////////////////////////////////////
+
+// var x = 2 ;
+// // x += 1 ;   //  x = x + 1   // 3
+// // x++ ;
+
+// // x -= 1 // x = x - 1
+// console.log(x)
+// //////////////////////////////////////////////////////
+
+// var x = 3 ;
+// var y = 4 ;
+// console.log ( x + y )
+
+// var x = 8;
+// var y = 12;
+// console.log ( x + y )
+
+// definition function
+// function  result() {
+//      var x = 3 ;
+//      var y = 4 ;
+//     console.log ( x + y )
+// }
+
+// // call function   trigger function
+// result() ;
+// /////////////////////////////////////////////////////
+
+// function result (x,y){    // arguments
+//    console.log(x*y)
+// }
+
+// result (8,9);
+// result (36,40);
+// result (6,9);
+// result (36,90);
+// result (150,140);
+///////////////////////////////////////////////////////
+// function result(){
+//     var z=9;
+//     return z;
+// }
+// result();
+// var x = result();
+// console.log (x)
+// //////////////////////////////////////////////////
+
+// var result = function (){
+//     console.log ("ahmed")
+// }
+// result();
+
+// var result2 = function test(){
+//     console.log ("ahmed")
+// }
+// result2();
+// /////////////////////////////////////////////////////
+// (function  code (){
+//     console.log ("ahmed")
+// })();
+
+// Encapsulation   IIFE    Immediately Invoked Function Expression
+
+// ////////////////////////////////////////////////////
+
+//Events // Onclick - ondblclick - onmouseenter  -  onmouseleave - onmousemove
+
+// var btn = document.getElementById("btn")
+// var div = document.getElementById("demo")
+
+// function action(){
+//     div.innerHTML ="Ahmed";
+// }
+//     btn.onmouseenter = action ;
+
+// ///////////////////////////////////////////////////
+
+// onkeypress   اول م يلاقى البوتون اضغط عليه -  onkeydown وانا داااااااااااااايس - onkeyup اضغط واشيل ايدى من على البوتون
+
+// var result = document.getElementById("input1");
+
+// result.onkeyup = function (){
+//     console.log("test")
+// }
+
+//////////////////////////////////////////////////////
+
+// window.onload = function (){
+//     alert("yes loaded")
+// }/
+
+////////////////////////////////////////////////////
